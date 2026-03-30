d733d851a3c9f3165ec487facf415e6de3a78d1b
Arrthos <108665505+Arrthos@users.noreply.github.com>
2026-03-30 19:48:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def calculate_due_fast(df_conges, solidarity_day):
     return df_dates['h_theo'].sum()
 
 # --- AUTH ---
-USERS = {"Julien": {"password": "123", "base_sup": 20.5}}
+USERS = {"Julien": {"password": "%Gfpass115", "base_sup": 20.5}}
 if 'authenticated' not in st.session_state: st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
