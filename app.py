761a98b1956f934515d6129b879adac454a9644e
Arrthos <108665505+Arrthos@users.noreply.github.com>
2026-03-30 17:52:21 +0200

    Refactor user configuration and login logic

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import holidays
 
 # --- 1. CONFIGURATION DES UTILISATEURS ---
 USERS = {
-    "Julien": {"password": "123", "base_sup": 20.5, "Regnault": "Julien", "role": "admin"},
+    "Julien": {"password": "123", "base_sup": 20.5, "full_name": "Julien", "role": "admin"},
     #"collegue1": {"password": "abc", "base_sup": 10.0, "full_name": "Jean Dupont", "role": "user"}
 }
 
